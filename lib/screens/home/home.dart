cc929252504b3e9b77d5a72aad67a3987e2973ab
lenny1400 <eliasjonas001@gmail.com>
2023-04-22 12:12:50 +0200

    Initial commit

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      physics: const PageScrollPhysics(),
+      child: Column(
+        children: const[
+
+        ],
+      ),
+    );
+  }
+}
